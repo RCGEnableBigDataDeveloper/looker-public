ed01cc6ca949ab212e6cde81e19b1cc1859f2e1d
eric perler <eric.perler@rcggs.com>
2022-01-03 13:57:23 -0500

    t

--- visualizations/js/test.js
+++ visualizations/js/test.js
@@ -1,65 +1,67 @@
-(function (dom, time_js) {
-  'use strict';
+'use strict';
 
-  function _interopNamespace(e) {
-    if (e && e.__esModule) return e;
-    var n = Object.create(null);
-    if (e) {
-      Object.keys(e).forEach(function (k) {
-        if (k !== 'default') {
-          var d = Object.getOwnPropertyDescriptor(e, k);
-          Object.defineProperty(n, k, d.get ? d : {
-            enumerable: true,
-            get: function () { return e[k]; }
-          });
-        }
-      });
-    }
-    n["default"] = e;
-    return Object.freeze(n);
-  }
+var dom = require('dom.js');
+var time_js = require('time.js');
 
-  var dom__namespace = /*#__PURE__*/_interopNamespace(dom);
-
-  // get clock element
-  const clock = dom__namespace.get('.clock');
-
-  if (clock) {
-
-    console.log('initializing clock');
-
-    // update clock every second
-    setInterval(() => {
-
-      clock.textContent = time_js.formatHMS();
-
-    }, 1000);
-
-  }
-
-  looker.plugins.visualizations.add({
-    // Id and Label are legacy properties that no longer have any function besides documenting
-    // what the visualization used to have. The properties are now set via the manifest
-    // form within the admin/visualizations page of Looker
-    id: "hello_world",
-    label: "Hello World",
-    options: {
-      font_size: {
-        type: "string",
-        label: "Font Size",
-        values: [
-          {"Large": "large"},
-          {"Small": "small"}
-        ],
-        display: "radio",
-        default: "large"
+function _interopNamespace(e) {
+  if (e && e.__esModule) return e;
+  var n = Object.create(null);
+  if (e) {
+    Object.keys(e).forEach(function (k) {
+      if (k !== 'default') {
+        var d = Object.getOwnPropertyDescriptor(e, k);
+        Object.defineProperty(n, k, d.get ? d : {
+          enumerable: true,
+          get: function () { return e[k]; }
+        });
       }
-    },
-    // Set up the initial state of the visualization
-    create: function(element, config) {
+    });
+  }
+  n["default"] = e;
+  return Object.freeze(n);
+}
 
-      // Insert a <style> tag with some styles we'll use later.
-      element.innerHTML = `
+var dom__namespace = /*#__PURE__*/_interopNamespace(dom);
+
+// get clock element
+const clock = dom__namespace.get('.clock');
+
+if (clock) {
+
+  console.log('initializing clock');
+
+  // update clock every second
+  setInterval(() => {
+
+    clock.textContent = time_js.formatHMS();
+
+  }, 1000);
+
+}
+
+looker.plugins.visualizations.add({
+  // Id and Label are legacy properties that no longer have any function besides documenting
+  // what the visualization used to have. The properties are now set via the manifest
+  // form within the admin/visualizations page of Looker
+  id: "hello_world",
+  label: "Hello World",
+  options: {
+    font_size: {
+      type: "string",
+      label: "Font Size",
+      values: [
+        {"Large": "large"},
+        {"Small": "small"}
+      ],
+      display: "radio",
+      default: "large"
+    }
+  },
+  // Set up the initial state of the visualization
+  create: function(element, config) {
+
+    // Insert a <style> tag with some styles we'll use later.
+    element.innerHTML = `
       <style>
         .hello-world-vis {
           /* Vertical centering */
@@ -78,43 +80,41 @@
       </style>
     `;
 
-      // Create a container element to let us center the text.
-      var container = element.appendChild(document.createElement("div"));
-      container.className = "hello-world-vis";
+    // Create a container element to let us center the text.
+    var container = element.appendChild(document.createElement("div"));
+    container.className = "hello-world-vis";
 
-      // Create an element to contain the text.
-      this._textElement = container.appendChild(document.createElement("div"));
+    // Create an element to contain the text.
+    this._textElement = container.appendChild(document.createElement("div"));
 
-    },
-    // Render in response to the data or settings changing
-    updateAsync: function(data, element, config, queryResponse, details, done) {
+  },
+  // Render in response to the data or settings changing
+  updateAsync: function(data, element, config, queryResponse, details, done) {
 
-      // Clear any errors from previous updates
-      this.clearErrors();
+    // Clear any errors from previous updates
+    this.clearErrors();
 
-      // Throw some errors and exit if the shape of the data isn't what this chart needs
-      if (queryResponse.fields.dimensions.length == 0) {
-        this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
-        return;
-      }
-
-      // Grab the first cell of the data
-      var firstRow = data[0];
-      var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
-
-      // Insert the data into the page
-      this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
-
-      // Set the size to the user-selected size
-      if (config.font_size == "small") {
-        this._textElement.className = "hello-world-text-small";
-      } else {
-        this._textElement.className = "hello-world-text-large";
-      }
-
-      // We are done rendering! Let Looker know.
-      done();
+    // Throw some errors and exit if the shape of the data isn't what this chart needs
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
+      return;
     }
-  });
 
-})(dom, time_js);
+    // Grab the first cell of the data
+    var firstRow = data[0];
+    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
+
+    // Insert the data into the page
+    this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
+
+    // Set the size to the user-selected size
+    if (config.font_size == "small") {
+      this._textElement.className = "hello-world-text-small";
+    } else {
+      this._textElement.className = "hello-world-text-large";
+    }
+
+    // We are done rendering! Let Looker know.
+    done();
+  }
+});
